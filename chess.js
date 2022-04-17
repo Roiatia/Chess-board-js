e02d0b27b940ddf37e81edaea4ddccd05bcf09a9
roiat <roiatia177@gmail.com>
2022-04-17 11:57:03 +0300

    minor changes

--- chess.js
+++ chess.js
@@ -1,5 +1,4 @@
 
-
 let chess = document.getElementById('chess')
 
 for(i = 1; i < 9; i++){
@@ -11,96 +10,5 @@ for(i = 1; i < 9; i++){
         box1.style.background = 'white'
     }
     chess.appendChild(box1)
-}
-
-
-for(i = 1; i < 9; i++){
-    let box2 = document.createElement('div')
-    if(i % 2 == 0){
-        box2.style.background = 'white'
-    }
-    else{
-        box2.style.background = 'black'
-    }
-    chess.appendChild(box2)
-
-}
-
-
-for(i = 1; i < 9; i++){
-    let box3 = document.createElement('div')
-    if(i % 2 == 0){
-        box3.style.background = 'black'
-    }
-    else{
-        box3.style.background = 'white'
-    }
-    chess.appendChild(box3)
-
-}
-
-
-for(i = 1; i < 9; i++){
-    let box4 = document.createElement('div')
-    if(i % 2 == 0){
-        box4.style.background = 'white'
-    }
-    else{
-        box4.style.background = 'black'
-    }
-    chess.appendChild(box4)
-
-}
-
-
-for(i = 1; i < 9; i++){
-    let box5 = document.createElement('div')
-    if(i % 2 == 0){
-        box5.style.background = 'black'
-    }
-    else{
-        box5.style.background = 'white'
-    }
-    chess.appendChild(box5)
-
-}
-
-
-for(i = 1; i < 9; i++){
-    let box6 = document.createElement('div')
-    if(i % 2 == 0){
-        box6.style.background = 'white'
-    }
-    else{
-        box6.style.background = 'black'
-    }
-    chess.appendChild(box6)
-
-}
-
-
-for(i = 1; i < 9; i++){
-    let box7 = document.createElement('div')
-    if(i % 2 == 0){
-        box7.style.background = 'black'
-    }
-    else{
-        box7.style.background = 'white'
-    }
-    chess.appendChild(box7)
-
-}
-
-
-for(i = 1; i < 9; i++){
-    let box8 = document.createElement('div')
-    if(i % 2 == 0){
-        box8.style.background = 'white'
-    }
-    else{
-        box8.style.background = 'black'
-    }
-    chess.appendChild(box8)
-
 }
 
